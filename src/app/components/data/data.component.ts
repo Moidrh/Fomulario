bab21a1a637aea06c165219f7af2095d85dc2211
Moises Rojas <moidrh@gmail.com>
2019-02-14 18:21:04 -0500

    Segundo commit - Se hace las validaciones a cada sección del formulario

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,10 +10,24 @@ export class DataComponent implements OnInit {
 
   forma: FormGroup;
 
+  usuario: Object = {
+    nombreCompleto: {
+      nombre: 'Moises',
+      apellido: 'Rojas'
+    },
+    correo: 'moises.rojas@gmail.com'
+  };
+
   constructor() {
+
+    console.log(this.usuario);
+
     this.forma = new FormGroup({
-      'nombre': new FormControl('', Validators.required),
-      'apellido': new FormControl('', Validators.required),
+
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl('', Validators.required),
+      }),
       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
   }
